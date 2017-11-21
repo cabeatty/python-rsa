5252e0305e9f25f6a2de2c160ca3ed23d3189818
cabeatty <cabeatty@oakland.edu>
2017-11-21 13:08:20 -0500

    pending tests, modular exponentiation and inversion are complete

---
+++ modmath.py
@@ -0,0 +1,31 @@
+def modularPow(base, exp, mod):
+    result = 1
+    while (exp > 0):
+        if (exp % 2 == 1):
+            result = (result * base) % mod
+            exp = exp >> 1
+            base = (base * base) % mod
+    return result;
+
+def modularExp(base, exp, mult, mod):
+    result = 1
+    baseMult = base*mult
+    while (exp > 0):
+        if (exp % 2 == 1):
+            result = (result * baseMult) % mod
+        exp = exp >> 1
+        baseMult = (baseMult * base) % mod
+    return result
+
+def modulo(n, p):
+    r = n%p;
+    if( ((p > 0) and (r < 0)) or ((p < 0) and (r > 0)) ):
+        r += p
+    return r
+
+def modInverse(n, mod):
+    n = modulo(n, mod)
+    for x in range(mod):
+        if(modulo(n*x, modulus) == 1):
+            return x
+    return 0
